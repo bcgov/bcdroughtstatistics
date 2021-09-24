29b297956e7de3daeae335527425eee5e0b1d890
Ashlee Jollymore <Ashlee.Jollymore@gov.bc.ca>
2021-09-24 16:34:01 -0700

    added annotation

--- R/utils.R
+++ R/utils.R
@@ -3,6 +3,8 @@
 # This work is licensed under the Creative Commons Attribution 4.0 International License.
 # To view a copy of this license, visit http://creativecommons.org/licenses/by/4.0/.
 
+# Function to get a new token if process takes too long (for tidyhydat.ws())
+
 start.time <- Sys.time()
 
 end.time <- Sys.time()
@@ -15,12 +17,3 @@ if(time.taken>=8*60){
 }
 
 
-#data_dir <- function() {
-#  # Use tools::R_user_dir on R >= 4.0, rappdirs otherwise.
-#  R_user_dir <- getNamespace("tools")$R_user_dir
-#  if (!is.null(R_user_dir)) {
-#    getOption("bcdroughtstats.data_dir", default = R_user_dir("bcdroughtstatistics", "cache"))
-#  } else {
-#    getOption("bcmaps.data_dir", default = rappdirs::user_cache_dir("bcmaps"))
-#  }
-#}
