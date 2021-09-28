dcadf5b4a805959e72416c3ee9aa020557162c5a
Ashlee Jollymore <Ashlee.Jollymore@gov.bc.ca>
2021-09-28 10:31:29 -0700

    Fixed path to RMarkdown file

--- R/render_bc_function.R
+++ R/render_bc_function.R
@@ -36,7 +36,8 @@ render_function_bc <- function(basin, save_loc = "directory") {
     save_loc_f = save_loc
   }
 
-  rmarkdown::render(paste0("inst/droughtstats_prov_template.Rmd"),
+  # Run RMarkdown file
+  rmarkdown::render(system.file("droughtstats_prov_template.Rmd", package = "bcdroughtstatistics"),
                     params = list(region = basin),
                     output_file = paste0(save_loc_f, gsub(" ", "", basin)  ,  ".html"))
 
